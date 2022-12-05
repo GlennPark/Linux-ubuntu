7eaa60d9f09b104530098805468d3eb28594dbaf
BBQ <42878943+GlennPark@users.noreply.github.com>
2022-12-05 18:22:18 +0900

    Add files via upload
    edgefilteradded

--- edgefilter.c
+++ edgefilter.c
@@ -1,66 +1,107 @@
-#include <stdio.h>
-#include <stdlib.h>
+﻿#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
+#include <limits.h>                     /* USHRT_MAX 상수를 위해서 사용한다. */
+#include <unistd.h>
 
+#include "bmpHeader.h"
 
+/* 이미지 데이터의 경계 검사를 위한 매크로 */
+#define LIMIT_UBYTE(n) ((n)>UCHAR_MAX)?UCHAR_MAX:((n)<0)?0:(n)
 
-//for(i = 0; i < 256; i++) histogram[i] = 0;
+typedef unsigned char ubyte;
 
-
-#define BYTE	unsigned char
-#define BASE    16
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-fread(&type, sizeof(unsigned short int), 1, fp);
-	fread(&file_size, sizeof(unsigned int), 1, fp);
-	fread(&reserved1, sizeof(unsigned short int), 1, fp);
-	fread(&reserved2, sizeof(unsigned short int), 1, fp);
-	fread(&offset, sizeof(unsigned int), 1, fp);
-	fread(&header_size, sizeof(unsigned int), 1, fp);
-	fread(&width, sizeof(int), 1, fp);
-	fread(&height, sizeof(int), 1, fp);
-	fread(&planes, sizeof(unsigned short int), 1, fp);
-	fread(&bits, sizeof(unsigned short int), 1, fp);
-	fread(&compression, sizeof(unsigned int), 1, fp);
-	fread(&imagesize, sizeof(unsigned int), 1, fp);
-	fread(&hresolution, sizeof(int), 1, fp);
-	fread(&vresolution, sizeof(int), 1, fp);
-	fread(&ncolors, sizeof(unsigned int), 1, fp);
-	fread(&importantcolors, sizeof(unsigned int), 1, fp);
+    FILE* fp;
+    BITMAPFILEHEADER bmpHeader;             /* BMP FILE INFO */
+    BITMAPINFOHEADER bmpInfoHeader;     /* BMP IMAGE INFO */
+    RGBQUAD* palrgb;
+    ubyte* inimg, * outimg;// *padding;
+    int x, y, z, imageSize;
 
-	size = widthbytes(bits * width);
-		if(!imagesize) imagesize = height * size;
-	inimg = (BYTE*)malloc(sizeof(BYTE)*imagesize);
-	outimg = (BYTE*)malloc(sizeof(BYTE)*imagesize);
-	fread(inimg, sizeof(BYTE), imagesize, fp);
-	fclose(fp);
-	for(i = 0; i < height; i++) {
-		index = (height-i-1) * size; 
-		for(j = 0 ; j < width; j++) { 
-			r = (float)inimg[index+3*j+2];
-			g = (float)inimg[index+3*j+1];
-			b = (float)inimg[index+3*j+0];
-			gray1 = (r*0.3F)+(g*0.59F)+(b*0.11F);
-		
-            r = (float)inimg[index+3*j+2-3];
-            g = (float)inimg[index+3*j+1-3];
-            b = (float)inimg[index+3*j+0-3];
-            gray1 = (r*0.3F)+(g*0.59F)+(b*0.11F);
-           
-			value = (int)(gray2 - gray1);
-			outimg[index + 3*j] = (value > BASE)? 255:0;
-			outimg[index + 3*j +1] = (value > BASE)? 255:0;
-			outimg[index + 3*j +2] = (value > BASE)? 255:0;
+    //Bitmap exe file
+    if (argc != 3) {
+        fprintf(stderr, "usage : %s input.bmp output.bmp\n", argv[0]);
+        return -1;
+    }
 
-			r = (float)inimg[index+3*j+2];
-            g = (float)inimg[index+3*j+1];
-            b = (float)inimg[index+3*j+0];
-            gray1 = (r*0.3F)+(g*0.59F)+(b*0.11F);
+    /***** read bmp *****/
+    if ((fp = fopen(argv[1], "rb")) == NULL) {
+        fprintf(stderr, "Error : Failed to open file...₩n");
+        return -1;
+    }
+
+    /* BITMAPFILEHEADER 구조체의 데이터 */
+    fread(&bmpHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+
+    /* BITMAPINFOHEADER 구조체의 데이터 */
+    fread(&bmpInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+
+    /* 트루 컬러를 지원하면 변환할 수 없다. */
+    if (bmpInfoHeader.biBitCount != 24) {
+        perror("This image file doesn't supports 24bit color\n");
+        fclose(fp);
+        return -1;
+    }
+
+    int elemSize = bmpInfoHeader.biBitCount / 8;
+    int size = bmpInfoHeader.biWidth * elemSize;
+    imageSize = size * bmpInfoHeader.biHeight;
+    int padding_size = (((bmpInfoHeader.biWidth + bmpInfoHeader.biHeight + 2) * elemSize) * 2);
+
+    /* 이미지의 해상도(넓이 × 깊이) */
+    printf("Resolution : %d x %d\n", bmpInfoHeader.biWidth, bmpInfoHeader.biHeight);
+    printf("Bit Count : %d\n", bmpInfoHeader.biBitCount);     /* 픽셀당 비트 수(색상) */
+    printf("Image Size : %d\n", imageSize);
+    printf("Padding Size : %d\n", padding_size);
+
+    inimg = (ubyte*)malloc(sizeof(ubyte) * imageSize + padding_size);
+    outimg = (ubyte*)malloc(sizeof(ubyte) * imageSize);
+    //   padding = (ubyte*)malloc(sizeof(ubyte)*imageSize+padding_size);
+    fread(inimg, sizeof(ubyte), imageSize + padding_size, fp);
+
+    fclose(fp);
+
+    // define the kernel
+    float kernel[3][3] = { {1 / 9.0, 1 / 9.0, 1 / 9.0},
+                           {1 / 9.0, 1 / 9.0, 1 / 9.0},
+                           {1 / 9.0, 1 / 9.0, 1 / 9.0} };
+    memset(outimg, 0, sizeof(ubyte) * imageSize);
+    for (y = 0; y < bmpInfoHeader.biHeight; y++) {
+        for (x = 0; x < (bmpInfoHeader.biWidth) * elemSize; x += elemSize) {
+            for (z = 0; z < elemSize; z++) {
+                float sum = 0.0;
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        sum += kernel[i + 1][j + 1] * inimg[(x - i * elemSize) + (y - j) * size + z];
+                    }
+                }
+                outimg[x + y * size + z] = sum;
+            }
+        }
+    }
+
+    /***** write bmp *****/
+    if ((fp = fopen(argv[2], "wb")) == NULL) {
+        fprintf(stderr, "Error : Failed to open file...₩n");
+        return -1;
+    }
+
+    /* BITMAPFILEHEADER 구조체의 데이터 */
+    fwrite(&bmpHeader, sizeof(BITMAPFILEHEADER), 1, fp);
+
+    /* BITMAPINFOHEADER 구조체의 데이터 */
+    fwrite(&bmpInfoHeader, sizeof(BITMAPINFOHEADER), 1, fp);
+
+    //fwrite(inimg, sizeof(ubyte), imageSize, fp); 
+    fwrite(outimg, sizeof(ubyte), imageSize, fp);
+
+    fclose(fp);
+
+    free(inimg);
+    free(outimg);
+    //   free(padding);
+
+    return 0;
 }
-
-
-
-
-//		};
-//	};
